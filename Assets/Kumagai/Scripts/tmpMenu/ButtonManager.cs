453f4bc69ef9fe2b7dc2245d763e025298ea00ad
kumagai <>
2024-05-01 18:52:15 +0900

    シーン選択後、確認を追加

--- Assets/Kumagai/Scripts/tmpMenu/ButtonManager.cs
+++ Assets/Kumagai/Scripts/tmpMenu/ButtonManager.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
     [SerializeField] private GameObject hole;
-    [SerializeField] private GameObject ground;
-    public static bool sceneChangeFlag;
+    [SerializeField] private GameObject sceneGround;
+    [SerializeField] private GameObject sceneCheckBackGround;
+    [SerializeField] private Text sceneName;
+    private bool sceneChangeFlag;
+    private string yesOrNo;
+    private string thisSceneName;
+    public static bool sceneCheck;
+
     
     // Start is called before the first frame update
     void Start()
     {
-        sceneChangeFlag = false;
+        sceneCheck = false;
+        yesOrNo = "No";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SceneCheck();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -27,7 +35,16 @@ public class ButtonManager : MonoBehaviour
         {
             if(PlayerMove.Drop)
             {
-                StartCoroutine(SceneChanger());
+                sceneCheck = true;
+                switch(this.gameObject.transform.name) 
+                {
+                    case "Main Game":
+                        {
+                            thisSceneName = "ÉÅÉCÉìÉQÅ[ÉÄ";
+                        }
+                        break;
+                }
+                sceneName.text = thisSceneName;
             }
         }
 
@@ -37,13 +54,40 @@ public class ButtonManager : MonoBehaviour
         }
     }
 
+    private void SceneCheck()
+    {
+        if(sceneCheck)
+        {
+            sceneCheckBackGround.SetActive(true) ;
+            if(Input.GetKeyDown(KeyCode.Z))
+            {
+                yesOrNo = "Yes";
+            }
+            if(Input.GetKeyDown(KeyCode.X))
+            {
+                yesOrNo = "No";
+                sceneCheck = false;
+            }
+        }
+        else
+        {
+            sceneCheckBackGround.SetActive(false);
+        }
+        if (yesOrNo=="Yes")
+        {
+            StartCoroutine(SceneChanger());
+        }
+       
+    }
+
 
     private IEnumerator SceneChanger()
     {
+        sceneCheckBackGround.SetActive(false);
         sceneChangeFlag = true;
         hole.SetActive(false);
         yield return new WaitForSeconds(1f);
-        ground.SetActive(false);
+        sceneGround.SetActive(false);
         this.gameObject.transform.GetComponent<SpriteRenderer>().color=new Color(0,0,0,0);  
         this.gameObject.transform.GetComponent<BoxCollider2D>().isTrigger = true;
         yield return new WaitForSeconds(2f);
